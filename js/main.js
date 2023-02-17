afe5c3efb22b6803853ea708e015e360317b21c4
DaLadnoVam <daladnovam2@gmail.com>
2023-02-17 13:27:05 +0200

    pizza-five

---
+++ js/main.js
@@ -0,0 +1,104 @@
+
+
+$(function() {
+	$('#fv-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+// opening shopping card
+const card = document.querySelector('#cart');
+const cardInfo = document.querySelector('#shopping-card');
+const btnClose = document.querySelector('#btn-close');
+
+card.addEventListener('click', () => {
+    cardInfo.classList.add('open')
+} )
+
+cardInfo.addEventListener('click', (event) => {
+    if(event.target.nodeName === 'A') {
+        cardInfo.classList.remove('open');
+      }
+})
+
+btnClose.addEventListener('click', () => {
+    cardInfo.classList.remove('open')
+});
+
+// библиотека перехода страниц
+/*
+barba.init({
+    views: [{
+      namespace: 'homepage',
+      beforeLeave(data) {
+        opacity: 0
+      }
+    }, {
+      namespace: 'menu',
+      beforeEnter(data) {
+        // do something before entering the `contact` namespace
+        opacity: 0
+      }
+    }]
+  });
+*/
+const formElSubmit = document.querySelector('.shopping-card');
+const nameEl = document.querySelector('[name="name"]');
+const telEl = document.querySelector('[name="tel"]');
+const streetEl = document.querySelector('[name="street"]');
+const houseEl = document.querySelector('[name="house"]');
+const apartmentEl = document.querySelector('[name="apartment"]');
+const entryEl = document.querySelector('[name="entry"]');
+const floorEl = document.querySelector('[name="floor"]');
+const btnSubmit = document.querySelector('[type="submit"]');
+
+nameEl.addEventListener('input', inputCheckNum);
+telEl.addEventListener('input', inputCheckLetter);
+streetEl.addEventListener('input', inputCheckNum);
+houseEl.addEventListener('input', inputCheckLetter);
+apartmentEl.addEventListener('input', inputCheckLetter);
+entryEl.addEventListener('input', inputCheckLetter);
+floorEl.addEventListener('input', inputCheckLetter);
+
+formElSubmit.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.currentTarget.reset();
+  event.currentTarget.style.reset()
+})
+
+function inputCheckNum (event) {
+  event.target.style.borderColor = 'green'
+  event.target.style.backgroundColor = '#FFFFFF'
+  if(event.target.value.match(/\d/g)) {
+    event.target.style.borderColor = 'red';
+    event.target.style.backgroundColor = 'red'
+  }
+};
+
+function inputCheckLetter(event) {
+  event.target.style.backgroundColor = '#FFFFFF'
+  event.target.style.borderColor = 'green'
+  if(event.target.value.match(/\D/g)) {
+    event.target.style.borderColor = 'red';
+    event.target.style.backgroundColor = 'red'
+  }
+};
+
+btnSubmit.addEventListener('click', () => {
+  window.location.href='ordered.html'
+})
+
+// ================================================ //
+
+const burgerEl = document.querySelector('.header__nav-menu');
+const btnBurger = document.querySelector('#burger');
+
+btnBurger.addEventListener('click', () => {
+  burgerEl.classList.toggle('open')
+})
---
+++ js/shop-card.js
@@ -0,0 +1,7 @@
+// const card = document.querySelector('#header__cart');
+
+// console.log(card)
+
+// card.addEventListener('click', event => {
+//     console.log(event.target)
+// } )
